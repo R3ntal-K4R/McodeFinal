71bb53401750512f492cd1a4bc3fd5abe1521e7c
Elijah <norton49383@gmail.com>
2025-09-22 10:40:30 -0500

    Added comments (to be removed), axis labs

--- plotters/threeDplotter.py
+++ plotters/threeDplotter.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # Not strictly needed in latest matplotlib, but safe to include
 
 
-def threeDPlotter(x, y, z):
+def threeDPlotter(x, y, z, xlab="X-axis", ylab="Y-axis", zlab="Z-axis"):
   """
     by triangulation of points
   """
@@ -16,4 +16,7 @@ def threeDPlotter(x, y, z):
   surf = ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
 
   ax.set_title("Triangulated 3D Surface from Scattered Data")
+  ax.set_xlabel(xlab)
+  ax.set_ylabel(ylab)
+  ax.set_zlabel(zlab)
   plt.show()
--- runSim.py
+++ runSim.py
@@ -10,46 +10,56 @@ from getTemp import getTemp, readSpecificHeat
 from readConstants import read_constants
 
 def read_params(fileName):
-    constants = {}
-    with open(fileName, 'r') as file:
+    constants = {} #constants all get put in a dictionary
+    with open(fileName, 'r') as file: #its taking constants.txt as the file
         for line in file:
             # Remove comments
-            line = line.split('#')[0].strip()
+            line = line.split('#')[0].strip() #if the first character is a hashtag...
             if not line:
-                continue
+                continue #you ignore it (only continue on not line)
             # Parse key and value
-            key, value = map(str.strip, line.split('='))
+            key, value = map(str.strip, line.split('=')) #string gets stripped and turned into key
+            #line is split after equals and is the value attatched to the key
             try:
                 value = float(value) if '.' in value or 'e' in value.lower() else int(value)
+                #floatify value, if theres a period or an e in it, otherwise intify it
             except ValueError:
                 pass  # Leave as string if it's not a number
-            constants[key] = value
-    return constants
+            constants[key] = value #saving the key value pairs to constats dictionary
+    return constants #returns the dictionary
 
 def main():
     parser = argparse.ArgumentParser(description="Run the simulation with a constants file.")
     parser.add_argument('-mc', type=str, required = True, help='Path to the material constants file')
-
+#i dont really know what this part does line by line but its what lets you plug it in and run it in terminal
     args = parser.parse_args()
     print(args)
     # --- Configuration ---
     simulation_script_name = 'ArgsRunSimulation.py'
+    #sets simulation_script_name to ArgsRunSimulation.py
     constants_file_name = args.mc
+    #constants_file_name is set to the args variable idk what the mc means it doesnt matter
     params_files_name = 'MVTparams.txt'
-
+    #params_files_name is set to MVTparams.txt
 
     # read params
     params = read_params(params_files_name)
+    #calls read_params based with MVTparams.txt as the file
     print(params)
     mass_start = params['mass_start'] # kg
     mass_end = params['mass_end']     # kg
     num_points = params['num_points']      # Number of simulation runs
     vy_start = params['vy_start']  #m/s
     vy_end = params['vy_end']  # m/s 
+    #these are all from MTVParams.txt
+    #it gets the value associated with the key and sets it to the variable
     
 
     vy_value_to_test = np.linspace(vy_start, vy_end, num_points) # spacer
+    #this spaces out the points from vy start to vy end based on num_points, which is 3 by default
     mass_values_to_test = np.linspace(mass_start, mass_end, num_points) # Use linspace for linear scale
+    #this spaces out the ponits for mass start and mass end 
+    #then it saves them to mass_values_to_test and vy_value_to_test
     
 
 
@@ -65,14 +75,19 @@ def main():
           f"Vy_value from {vy_start} to {vy_end}") # Updated message
     # print(f"Ensure '{constants_file_name}' contains a fixed 'V_Y_initial'.")
     constants = read_constants(constants_file_name)
+    #refer to readConstants file, its reading them and saving them to constants
     material_name = constants['material_name']
+    #material name is from constants (its a dict)
     material_specific_heat = readSpecificHeat(material_name)
+    #refer to get temp file
+    #gets the specific heat of 
 
     for vy in vy_value_to_test:
+        #these are the spaced out values from earlier
 
         for mass in mass_values_to_test:
             energy_loss,max_displacement = run_single_simulation(mass, vy, constants_file_name, simulation_script_name)
-
+            #refer to singlesimulation file
             if energy_loss or max_displacement is not None:
                 results_mass.append(mass)
                 results_vy.append(vy)
@@ -106,9 +121,10 @@ def main():
         np.savetxt(f"./OutputData/{title}.csv", points, delimiter=",", header="mass,v_y,Eloss,Dmax,dT", comments='')
         print(f"\nCollected {len(results_mass_np)}, {len(results_v_y_np)} results. Generating plot...")
         # plotter(results_mass_np,results_energy_loss_magnitude)
-        print(len(results_mass_np),len(results_v_y_np), len(results_energy_loss_magnitude),len(results_max_displacement))
-        threeDPlotter(results_mass_np,results_v_y_np,results_energy_loss_magnitude)
-        threeDPlotter(results_mass_np,results_v_y_np,results_max_displacement)
+        print(len(results_mass_np),len(results_v_y_np), len(results_energy_loss_magnitude),len(results_max_displacement), len(results_max_displacement))
+        threeDPlotter(results_mass_np,results_v_y_np,results_energy_loss_magnitude, "Mass (kg)", "Velocity (m/s)", "Energy Loss (J)")
+        threeDPlotter(results_mass_np,results_v_y_np,results_max_displacement, "Mass (kg)", "Velocity (m/s)", "Max Displacement (m)")
+        threeDPlotter(results_mass_np,results_v_y_np,results_temp_change, "Mass (kg)", "Velocity (m/s)", "Temperature Change (K)")
 
 
 
